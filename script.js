dcebacd16e39df3636c73f36e2b4d37dc20fa86a
Ali Mahmmoud <127209252+Ali2653766@users.noreply.github.com>
2025-05-11 21:31:38 +0000

    Added projects section

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ resumeBtns.forEach((btn, idx) => {
     
     });
 });
+
+
+const arrowright = document.querySelector('.portofolio-box .navigation .arrow-right');
+const arrowleft = document.querySelector('.portofolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.img-slide');
+    const portfolioDetails = document.querySelectorAll('.portofolio-details');
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active');
+};
+
+arrowright.addEventListener('click', () => {
+    if (index < 1) {
+        index++;
+        arrowleft.classList.remove('disabled');
+    }
+    else {
+        index = 2;
+        arrowright.classList.add('disabled');
+    }
+    activePortfolio();
+});
+
+arrowleft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowright.classList.remove('disabled');
+    }
+    else {
+        index = 0;
+        arrowleft.classList.add('disabled');
+    }
+    activePortfolio();
+});
